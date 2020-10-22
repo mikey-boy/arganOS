c546c50f3862d1a7783611aefd02e1423c83c398
mikey-boy <michael.argany@protonmail.com>
2020-10-22 11:42:27 -0400

    Added kernel printf functionality

--- src/kernel.c
+++ src/kernel.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+#include "multiboot2.h"
 
 /* Check if the compiler thinks you are targeting the wrong operating system. */
 #if defined(__linux__)
@@ -50,6 +51,12 @@ size_t strlen(const char* str)
     return len;
 }
 
+void strcpy(const char * src, char * dest) {
+    dest[0] = src[0];
+    for (int i = 1; src[i-1]; i++)
+        dest[i] = src[i];
+}
+
 static const size_t VGA_WIDTH = 80;
 static const size_t VGA_HEIGHT = 25;
 
@@ -79,6 +86,7 @@ void terminal_setcolor(uint8_t color)
 
 void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 {
+    if (c == '\n') { return; }
     const size_t index = y * VGA_WIDTH + x;
     terminal_buffer[index] = vga_entry(c, color);
 }
@@ -86,7 +94,7 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 void terminal_putchar(char c)
 {
     terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-    if (++terminal_column == VGA_WIDTH) {
+    if (++terminal_column == VGA_WIDTH || c == '\n') {
         terminal_column = 0;
         if (++terminal_row == VGA_HEIGHT)
             terminal_row = 0;
@@ -104,11 +112,101 @@ void terminal_writestring(const char* data)
     terminal_write(data, strlen(data));
 }
 
+/* ntos - Convert interger to string representation of any base (up to 16)
+ *
+ * buf - array to store the result
+ * num - number to convet
+ * base - desired number base
+ */
+void ntos(char * buf, int num, int base) {
+    char conv[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                    'A', 'B', 'C', 'D', 'E', 'F'};
+
+    if (num == 0) {
+        buf[0] = '0';
+        buf[1] = 0;
+        return;
+    }
+
+    /* Divide by div until num == 0 */
+    int i = 0;
+    while (num > 0){
+        buf[i] = conv[num % base];
+        num = num / base;
+        i++;
+    }
+
+    buf[i] = 0;
+    i--;
+
+    /* Reverse the buffer */
+    int j = 0;
+    while (j<i) {
+        char c = buf[j];
+        buf[j] = buf[i];
+        buf[i] = c;
+        j++;
+        i--;
+    }
+}
+
+/* printf - Print a format string. Supports %x, %d, %o, %b, and %s.
+ *
+ * format - the format string
+ * ... - values corresponding to format string. Args need to be on the stack.
+ */
+void printf(const char *format, ...)
+{
+    char c;
+    char buf[20];
+
+    char ** arg = (char **) &format;
+    arg++;
+
+    while ((c = *format++) != 0) {
+        if (c != '%') {
+            terminal_putchar(c);
+            continue;
+        }
+
+        /* Handle format character */
+        c = *format++;
+
+        if (c != 'x' && c != 'd' && c != 'o' && c != 'b' && c != 's')
+            continue;
+        switch (c) {
+            case 'x':
+                ntos(buf, *((int *) arg) , 16);
+                break;
+            case 'd':
+                ntos(buf, *((int *) arg) , 10);
+                break;
+            case 'o':
+                ntos(buf, *((int *) arg) , 8);
+                break;
+            case 'b':
+                ntos(buf, *((int *) arg) , 2);
+                break;
+            case 's':
+                strcpy(*arg, buf);
+                break;
+        }
+        terminal_writestring(buf);
+        arg++;
+    }
+}
+
 void kernel_main(unsigned long magic, unsigned long addr)
 {
+    /* Am I booted by a multiboot2 compliant bootloader? */
+    if (magic != MULTIBOOT_BOOTLOADER_MAGIC) {
+        printf("Invalid magic number: 0x%x\n", magic);
+        return;
+    }
+
     /* Initialize terminal interface */
     terminal_initialize();
 
     /* Newline support is left as an exercise. */
-    terminal_writestring("Hello, kernel World!\n");
+    printf("Hello kernel world!\n");
 }
